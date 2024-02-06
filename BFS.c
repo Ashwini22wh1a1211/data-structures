ad8fa0b949694ea09854420db49b7588c7393f4c
Ashwini22wh1a1211 <121919297+Ashwini22wh1a1211@users.noreply.github.com>
2024-02-06 17:35:49 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -1,42 +1,50 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #define MAX 5 
-void DFS(int adj[][MAX], int visited[], int start); 
+ 
+void BFS(int adj[][MAX], int visited[], int start); 
+ 
 int main() 
 { 
-int visited[MAX] = {0}; 
-int adj[MAX][MAX], i, j; 
-int ch, size; 
-
-printf("Enter the values into the adjacency matrix\n"); 
-for (i = 0; i < MAX; i++) 
-{ 
-for (j = 0; j < MAX; j++) 
-scanf("%d", &adj[i][j]); 
+    int visited[MAX] = {0}; 
+    int adj[MAX][MAX], i, j; 
+    int ch, size; 
+ 
+    printf("Enter the values into the adjacency matrix\n"); 
+    for (i = 0; i < MAX; i++) 
+    { 
+        for (j = 0; j < MAX; j++) 
+            scanf("%d", &adj[i][j]); 
+    } 
+ 
+    printf("BFS Traversal is\n"); 
+    BFS(adj, visited, 0); 
+ 
+    return 0; 
 } 
-printf("DFS Traversal is\n"); 
-DFS(adj, visited, 0); 
-return 0; 
-} 
-void DFS(int adj[][MAX], int visited[], int start) 
+ 
+void BFS(int adj[][MAX], int visited[], int start) 
 { 
-int s[MAX]; 
-int top = -1, i, k; 
-for (k = 0; k < MAX; k++) 
-visited[k] = 0; 
-s[++top] = start; 
-visited[start] = 1; 
-while (top != -1) 
-{ 
-start = s[top--]; 
-printf("%c--", start + 65); 
-for (i = 0; i < MAX; i++) 
-{ 
-if (adj[start][i] && visited[i] == 0) 
-{ 
-s[++top] = i;
-visited[i] = 1; 
-}
-}
-}
+    int q[MAX], rear = -1, front = -1, i, k; 
+ 
+    for (k = 0; k < MAX; k++) 
+        visited[k] = 0; 
+ 
+    q[++rear] = start; 
+    ++front; 
+    visited[start] = 1; 
+ 
+    while (rear >= front) 
+    { 
+        start = q[front++]; 
+        printf("%c--", start + 65); 
+        for (i = 0; i < MAX; i++) 
+        { 
+            if (adj[start][i] == 1 && visited[i] == 0) 
+            { 
+                q[++rear] = i; 
+                    visited[i] = 1; 
+                } 
+            }
+        }
 }
